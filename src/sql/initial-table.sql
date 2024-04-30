6d4b49c5177dbeb3cee8b919338cdaaeb59d6b65
DatJoshLife <datjoshlife@gmail.com>
2024-04-29 21:57:57 -0600

    Varchar values added, SQL statements added tef dekcug

--- src/sql/initial-table.sql
+++ src/sql/initial-table.sql
@@ -1,7 +1,7 @@
 create table absent_players (
 user_id int NOT NULL,
-user_name varchar NOT NULL,
+user_name varchar(20) NOT NULL,
 date_ab date NOT NULL,
-reason_ab varchar,
+reason_ab varchar(200),
 has_passed int DEFAULT 0);
 /
