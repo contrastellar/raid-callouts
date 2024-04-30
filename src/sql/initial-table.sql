a4b1a6e1d25a7806a9ed5509849fefa5ed60d51a
DatJoshLife <datjoshlife@gmail.com>
2024-04-29 21:44:33 -0600

    Table 2 axed, table 1 clarified, fet gucked

--- src/sql/initial-table.sql
+++ src/sql/initial-table.sql
@@ -1,10 +1,7 @@
 create table absent_players (
-user_id int,
-user_name varchar,
-date_ab date,
-reason_ab varchar);
-
-create table absence_log(
-user_id int,
-number_absent int);
+user_id int NOT NULL,
+user_name varchar NOT NULL,
+date_ab date NOT NULL,
+reason_ab varchar,
+has_passed int DEFAULT 0);
 /
