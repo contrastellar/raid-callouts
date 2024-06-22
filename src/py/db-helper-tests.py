8511b5efb30f50068847ef9c65fdb510918a1bd8
Gabriella Stimac <gstimac@msudenver.edu>
2024-06-22 13:18:15 -0600

    Created test suite for `db_helper.py`

---
+++ src/py/db-helper-tests.py
@@ -0,0 +1,36 @@
+"""
+Database functionality tests.
+
+@author: Gabriella "Contrastellar" Agathon
+"""
+
+import datetime
+import pytest
+import helper.db_helper
+
+class TestClass():
+    """
+    Test class for the database functionality.
+    These tests should only be assertions of type, not of return values.
+    """
+    DATABASE_CONN = helper.db_helper.DBHelper()
+
+
+    def test_add_callout(self) -> None:
+        callout = self.DATABASE_CONN.add_callout(user_id=1, callout=datetime.date.today(), reason='test', nickname='test')
+        assert callout is None
+    
+
+    def test_callouts(self) -> None:
+        callout = self.DATABASE_CONN.query_callouts(days=7)
+        assert callout is not None
+
+
+    def test_remove_callout(self) -> None:
+        remove_callout = self.DATABASE_CONN.remove_callout(user_id=1, callout=datetime.date.today())
+        assert remove_callout is None
+
+    def test_format_list_of_callouts(self) -> None:
+        callouts = self.DATABASE_CONN.query_callouts(days=7)
+        formatted_callouts = self.DATABASE_CONN.format_list_of_callouts(callouts=callouts)
+        assert formatted_callouts.__class__ is str
