615ff53b96f2e3ab035088994264a4def099a50b
Gabriella Stimac <gstimac@msudenver.edu>
2024-11-23 12:26:30 -0700

    Edited tests to properly function

--- src/py/db-helper-tests.py
+++ src/py/db-helper-tests.py
@@ -13,11 +13,15 @@ class TestClass():
     Test class for the database functionality.
     These tests should only be assertions of type, not of return values.
     """
-    DATABASE_CONN = helper.db_helper.DBHelper()
+    DATABASE_CONN = helper.db_helper.DBHelper("xiv-database.ini")
+
+    def test_add_registration(self) -> None:
+        registration = self.DATABASE_CONN.register_char_name(uid=1, char_name="test")
+        assert registration is None
 
 
     def test_add_callout(self) -> None:
-        callout = self.DATABASE_CONN.add_callout(user_id=1, callout=datetime.date.today(), reason='test', nickname='test')
+        callout = self.DATABASE_CONN.add_callout(user_id=1, callout=datetime.date.today(), reason='test', nickname='test', char_name='test')
         assert callout is None
     
 
@@ -29,6 +33,10 @@ class TestClass():
     def test_remove_callout(self) -> None:
         remove_callout = self.DATABASE_CONN.remove_callout(user_id=1, callout=datetime.date.today())
         assert remove_callout is None
+
+    def test_remove_registration(self) -> None:
+        registration = self.DATABASE_CONN.remove_registration(uid=1)
+        assert registration is None
 
     def test_format_list_of_callouts(self) -> None:
         callouts = self.DATABASE_CONN.query_callouts(days=7)
