c72ea3e90cf0e3d906e80ae0d6e7940b88c2c9ce
Gabriella Agathon <gstimac@msudenver.edu>
2024-11-24 21:57:39 -0700

    Format edit to the tests

--- src/py/db-helper-tests.py
+++ src/py/db-helper-tests.py
@@ -18,9 +18,11 @@ class TestClass():
         registration = self.DATABASE_CONN.register_char_name(uid=1, char_name="test")
         assert registration is None
 
+
     def test_query_callouts(self) -> None:
         callouts = self.DATABASE_CONN.query_callouts(7)
         assert type(callouts) is list
+
 
     def test_add_callout(self) -> None:
         callout = self.DATABASE_CONN.add_callout(user_id=1, callout=datetime.date.today(), reason='test', nickname='test', char_name='test')
@@ -36,14 +38,17 @@ class TestClass():
         remove_callout = self.DATABASE_CONN.remove_callout(user_id=1, callout=datetime.date.today())
         assert remove_callout is None
 
+
     def test_char_name(self) -> None:
         char_name = self.DATABASE_CONN.return_char_name(uid=1)
         assert type(char_name) is str
         assert char_name == "test"
 
+
     def test_remove_registration(self) -> None:
         registration = self.DATABASE_CONN.remove_registration(uid=1, isOkay=True)
         assert registration is None
+
 
     def test_format_list_of_callouts(self) -> None:
         callouts = self.DATABASE_CONN.query_callouts(days=7)
